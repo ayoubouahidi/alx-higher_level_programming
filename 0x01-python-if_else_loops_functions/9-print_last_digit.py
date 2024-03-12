708692814abe92debdb23f0b00aa627254227809
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-03-12 21:48:17 +0000

    file last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number % 10 
+    if number < 0:
+        number *= -1
+    a = number % 10
+    if number == 1:
+        number *= -1
+    print("{:d}".format(a), end="")
+    return a
