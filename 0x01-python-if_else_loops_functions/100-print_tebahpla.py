136b77b9db5f3d6c4a229bc264930b8c6af5fcd1
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-03-12 22:24:24 +0000

    erreur fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:s}".format(chr(i)), end="")
+k = 122
+while k >= 97:
+    x = 0
+    if k % 2 != 0:
+        k = k - 32
+        x = 1
+    print("{:s}".format(chr(k)), end="")
+    if x == 1:
+        k = k + 32
+    k = k - 1
