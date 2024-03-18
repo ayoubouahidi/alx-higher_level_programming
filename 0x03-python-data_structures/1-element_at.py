1d927090aa097be7bf45e35f1dd03777f710553f
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-03-18 18:36:16 +0000

    data structure

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0:
-            return (my_list[idx] if 0 <= idx < len(my_list) else None)
+def element_at(my_list, idx):    
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
