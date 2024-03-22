6c2ca6507a7dc3a7875845eeb2fe356dccd5710b
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-03-22 23:00:19 +0000

    more data

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
   M=[]
-  for i in range(3):
+  for i in range(len(matrix)):
     L=[]
-    for j in range(3):
+    for j in range(len(matrix)):
       r = matrix[i][j]*matrix[i][j]
       L.append(r)
     M.append(L)
