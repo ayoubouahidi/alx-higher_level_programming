d8f28ee74634202c0c1002fc6f7224553a9ea258
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-03-23 00:46:27 +0000

    more data

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     li = [] 
     for i in my_list:
         if search == i:
-             li.append(replace)
+            li.append(replace)
         else:
             li.append(i)
     return li
